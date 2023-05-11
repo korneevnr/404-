475621a070a50874388203f913c681ef9b36b00e
CatatonicChimp1 <114211884+CatatonicChimp1@users.noreply.github.com>
2023-05-11 22:29:20 +0300

    Add files via upload

---
+++ src/test/java/com/example/serverIntelliTestinh/ServerIntelliTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverIntelliTestinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerIntelliTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
